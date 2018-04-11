10a316fb18dcad841c3c60a708a1c755ea07d404
Olivier Monnier <omonnier.dev@gmail.com>
2018-04-11 09:35:38 +0200

    in progress

--- src/utils/media.js
+++ src/utils/media.js
@@ -2,7 +2,6 @@ import { desktopCapturer } from 'electron';
 import settingsDefault from './settingsDefault';
 const Store = require('electron-store');
 const store = new Store({ defaults: settingsDefault });
-const rtcConfig = store.get('rtc');
 
 export function getSources() {
   return new Promise((resolve, reject) => {
@@ -15,28 +14,17 @@ export function getSources() {
 
 export function getUserMedia(config = {}) {
   const { source } = config;
-  const video = getVideoConfig();
+  const { audio, video } = store.get('media');
   let { mandatory } = video;
 
-  if (source && source !== "vr") {
+  if (source) {
     mandatory['chromeMediaSourceId'] = source;
   }
 
   return navigator.mediaDevices.getUserMedia({ 
-    audio: rtcConfig.audio, 
+    audio, 
     video: {
       mandatory
     }
   })
-}
-
-function getVideoConfig() {
-  const { maxWidth, maxHeight, minWidth, minHeight, maxFrameRate } = rtcConfig
-
-  return {
-    mandatory: {
-      chromeMediaSource: 'desktop',
-      maxWidth, maxHeight, minWidth, minHeight, maxFrameRate
-    }
-  }
 }
--- src/utils/settingsDefault.js
+++ src/utils/settingsDefault.js
@@ -1,12 +1,17 @@
 export default {
   debugMod: false,
-  rtc: {
+  media: {
     audio: false,
-    maxHeight: 1200,
-    maxWidth: 1980,
-    minHeight: 180,
-    minWidth: 320,
-    maxFrameRate: 60
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        maxHeight: 1200,
+        maxWidth: 1980,
+        minHeight: 180,
+        minWidth: 320,
+        maxFrameRate: 60
+      }
+    }
   },
   server: {
     host: 'http://127.0.0.1:8080'
